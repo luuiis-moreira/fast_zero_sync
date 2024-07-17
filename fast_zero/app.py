6f7259c2d8581d24193469a74d842221275669e8
Luis Reinaldo <luis.moreira.oliveira@gmail.com>
2024-07-16 23:27:17 -0300

    Implementando o CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,62 @@
-from fastapi import FastAPI
-from schemas import Message
 from http import HTTPStatus
 
+from fastapi import FastAPI, HTTPException
+
+from schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
 app = FastAPI()
+
+db = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Ola Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(db) + 1, **user.model_dump())
+
+    db.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': db}
+
+
+@app.get('/user/{user_id}', response_model=UserPublic)
+def get_user(user_id: int):
+    if user_id < 1 or user_id > len(db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found!'
+        )
+    user_with_id = db[user_id - 1]
+
+    return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found!'
+        )
+
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    db[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found!'
+        )
+    del db[user_id - 1]
+
+    return {'message': 'User Deleted!'}
